8e499f4cd7fd89c5e3ceec48cbf5744234b18a9e
mike14747 <mike4747@oh.rr.com>
2021-10-06 09:56:56 -0400

    Moved header and footer styles to their own files.

--- js/app.js
+++ js/app.js
@@ -16,7 +16,7 @@ function renderData(data, stateName) {
             <p class=description>${park.description}</p>
             <div class="img-container"><img class="image" src="${park.images[0].url}" alt="${park.images[0].altText}" /></div>
             <p class="location"><strong>Location:</strong> ${convertGPS(park.latitude, 'latitude')} ${convertGPS(park.longitude, 'longitude')}</p>
-            <div class="activities"><strong><span class="activities-text">Activities:</span></strong>
+            <div class="activities"><span class="activities-text">Activities:</span>
                 <ul>
                     ${park.activities.slice(0, 3).map(a => '<li>' + a.name + '</li>').join('')}
                 </ul>
