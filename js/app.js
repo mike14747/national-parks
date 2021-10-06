e4757dbfa85c6f5896c8dea92e73bb925fbc16a2
mike14747 <mike4747@oh.rr.com>
2021-10-06 03:02:01 -0400

    Finished the header, footer and hero section of index.html.

--- js/app.js
+++ js/app.js
@@ -1,43 +1,43 @@
-document.getElementById("state").innerHTML = stateList.map(state => `<option value="${state.value}">${state.name}</option>`).join('');
+document.getElementById("state").innerHTML = stateList.map(state => `<option data-name="${state.name}" value="${state.value}" ${state.value === 'ca' && 'selected=selected'}>${state.name}</option>`).join('');
 
 const apiKey = 'ldPNGdkCg4MooP2qY2V5QBBwZb3nfaoFeSCij7uL';
 
 const convertGPS = (deg, dir) => Math.abs(parseFloat(deg)).toFixed(3) + ' ' + (dir === 'latitude' ? deg >= 0 ? 'N' : 'S' : deg >= 0 ? 'E' : 'W');
 
-function renderData(data) {
+function renderData(data, stateName) {
     if (!data) return;
-    console.log(data.data);
+    console.log(data);
 
     let mappedDiv = '';
 
-    data.data.map(park => {
+    data.map(park => {
         mappedDiv += `<div class="card">
             <h3>${park.fullName}</h3>
             <p class=description>${park.description}</p>
-            <div><img class="image" src="${park.images[0].url}" alt="${park.images[0].altText}" /></div>
-            <p><strong>Location:</strong> ${convertGPS(park.latitude, 'latitude')} ${convertGPS(park.longitude, 'longitude')}</p>
-            <div><strong>Activities:</strong>
+            <div class="img-container"><img class="image" src="${park.images[0].url}" alt="${park.images[0].altText}" /></div>
+            <p class="location"><strong>Location:</strong> ${convertGPS(park.latitude, 'latitude')} ${convertGPS(park.longitude, 'longitude')}</p>
+            <div class="activities"><strong><span class="activities-text">Activities:</span></strong>
                 <ul>
                     ${park.activities.slice(0, 3).map(a => '<li>' + a.name + '</li>').join('')}
                 </ul>
             </div>
-            <p><a href="${park.url}" target="_blank">More Info</a></p>
+            <p class="info"><a href="${park.url}" target="_blank"><span class="pointer">More Info</span></a></p>
         </div>`;
     });
 
     document.getElementById('root').innerHTML = mappedDiv;
+    document.getElementById('viewing').textContent = stateName;
 }
-
-// "43.496 N 70.449 W"
 
 async function fetchData() {
     const stateCode = document.getElementById("state").value;
+    const stateName = document.querySelector(`[value=${stateCode}]`).getAttribute('data-name');
     const url = 'https://developer.nps.gov/api/v1/parks?stateCode=' + stateCode + '&limit=12&api_key=';
 
     try {
         const response = await fetch(url + apiKey);
         const data = await response.json();
-        renderData(data);
+        renderData(data?.data, stateName);
     } catch (error) {
         console.log(error)
     }
